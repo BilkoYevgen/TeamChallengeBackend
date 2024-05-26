aecac6da3c9629a58c8043f638b425763ea8e93b
BIlko Yevgen <113464448+BilkoYevgen@users.noreply.github.com>
2024-05-26 18:08:34 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsu
 if [[ $CREATE_SUPERUSER ]]; then
