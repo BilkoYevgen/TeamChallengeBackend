4d9a79a66c141c0b587b011cb17e8eceffc2e1ca
Stepan Veklych <grandma7ter500@gmail.com>
2024-05-29 16:20:34 +0200

    Admin representation fix

--- product/models.py
+++ product/models.py
@@ -10,12 +10,18 @@ class Category(models.Model):
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class SubCategory(models.Model):
     name = models.CharField(_('name'), max_length=80)
     category = models.ForeignKey(
         Category, verbose_name=_('category name'), related_name='categories', on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return f'{self.name}'
 
     class Meta:
         verbose_name = _('Subcategory')
